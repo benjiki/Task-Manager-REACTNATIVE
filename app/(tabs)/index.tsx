ffa46b2eb916c72414a1c560d11a50fca48fd8f1
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-08-15 02:08:15 +0300

    progress bar and some other stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
 import { useTheme } from "@/hooks/useTheme";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar, Text, TouchableOpacity } from "react-native";
@@ -13,6 +14,7 @@ export default function Index() {
     >
       <StatusBar barStyle={colors.statusBarStyle} />
       <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
         <TouchableOpacity onPress={toggleDarkMode}>
           <Text>Dark mode</Text>
         </TouchableOpacity>
