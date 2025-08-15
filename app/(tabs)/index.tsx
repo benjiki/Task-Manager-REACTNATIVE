1c47f48757be93ae23a76704f35ee07358a96b61
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-08-16 00:44:38 +0300

    input for tasks added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import Todoinput from "@/components/Todoinput";
 import { useTheme } from "@/hooks/useTheme";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar, Text, TouchableOpacity } from "react-native";
@@ -15,6 +16,7 @@ export default function Index() {
       <StatusBar barStyle={colors.statusBarStyle} />
       <SafeAreaView style={homeStyles.safeArea}>
         <Header />
+        <Todoinput />
         <TouchableOpacity onPress={toggleDarkMode}>
           <Text>Dark mode</Text>
         </TouchableOpacity>
